c4e41c84cbf4d8bf5b2f2227f3bd843fe394f4e4
Curtis Summers <curtis.summers@layerware.com>
2015-10-23 06:25:21 -0500

    Fix leiningen dependency issue #2

--- hugsql-core/project.clj
+++ hugsql-core/project.clj
@@ -1,14 +1,14 @@
-(defproject com.layerware/hugsql-core "0.2.0"
+(defproject com.layerware/hugsql-core "0.2.1"
   :description "hugsql core functionality without adapter dependencies"
   :url "https://github.com/layerware/hugsql"
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [com.layerware/hugsql-adapter "0.1.1"]]
+                 [com.layerware/hugsql-adapter "0.2.1"]]
   :profiles {:dev
              {:plugins [[lein-auto "0.1.2"]]
-              :dependencies [[com.layerware/hugsql-adapter-clojure-java-jdbc "0.2.0"]
-                             [com.layerware/hugsql-adapter-clojure-jdbc "0.2.0"]
+              :dependencies [[com.layerware/hugsql-adapter-clojure-java-jdbc "0.2.1"]
+                             [com.layerware/hugsql-adapter-clojure-jdbc "0.2.1"]
                              [org.postgresql/postgresql "9.4-1201-jdbc41"]
                              [mysql/mysql-connector-java "5.1.37"]
                              [org.xerial/sqlite-jdbc "3.8.11.2"]
