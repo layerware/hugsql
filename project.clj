d263e7d0ec37992c7a13270754e47b970cdd7046
Curtis Summers <curtis.summers@layerware.com>
2015-11-11 16:24:10 -0600

    Add lein alias for local jar install of all hugsql sub projects

--- project.clj
+++ project.clj
@@ -6,6 +6,7 @@
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [com.layerware/hugsql-core "0.3.0-SNAPSHOT"]
                  [com.layerware/hugsql-adapter-clojure-java-jdbc "0.3.0-SNAPSHOT"]]
+  :aliases {"install-all" ["do" "install" ["sub" "install"]]}
   :profiles {:dev
              {:plugins [[lein-codox "0.9.0"]
                         [lein-sub "0.3.0"]]
@@ -14,7 +15,7 @@
                     "hugsql-adapter-clojure-java-jdbc"
                     "hugsql-adapter-clojure-jdbc"]
               :dependencies [[com.layerware/hugsql-adapter-clojure-jdbc "0.3.0-SNAPSHOT"]]
-              :codox {:source-uri "http://github.com/layerware/hugsql/blob/0.2.2/{filepath}#L{line}"
+              :codox {:source-uri "http://github.com/layerware/hugsql/blob/0.3.0/{filepath}#L{line}"
                       :output-path "../gh-pages"
                       :source-paths ["hugsql-core/src"
                                      "hugsql-adapter/src"
