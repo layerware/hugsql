b5f3a9625eec9b76debc85987161ca23cfb6091d
Curtis Summers <curtis.summers@layerware.com>
2015-12-02 06:21:09 -0600

    Version 0.3.1

--- examples/princess-bride/project.clj
+++ examples/princess-bride/project.clj
@@ -1,4 +1,4 @@
-(defproject princess-bride "0.3.0"
+(defproject princess-bride "0.3.1"
   :description "The Princess Bride HugSQL Example App"
   :url "https://github.com/layerware/hugsql"
   :license {:name "Apache License, Version 2.0"
@@ -6,5 +6,5 @@
   :main princess-bride.core
   :aliases {"as-you-wish" ["run"]}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [com.layerware/hugsql "0.3.0"]
+                 [com.layerware/hugsql "0.3.1"]
                  [com.h2database/h2 "1.4.190"]])
--- hugsql-adapter-clojure-java-jdbc/project.clj
+++ hugsql-adapter-clojure-java-jdbc/project.clj
@@ -1,8 +1,8 @@
-(defproject com.layerware/hugsql-adapter-clojure-java-jdbc "0.3.0"
+(defproject com.layerware/hugsql-adapter-clojure-java-jdbc "0.3.1"
   :description "clojure.java.jdbc hugsql adapter"
   :url "https://github.com/layerware/hugsql"
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/java.jdbc "0.4.2"]
-                 [com.layerware/hugsql-adapter "0.3.0"]])
+                 [com.layerware/hugsql-adapter "0.3.1"]])
