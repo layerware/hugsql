233b1d442ffbfc9fa022d00833c2b16b4f1fc32d
Curtis Summers <curtis.summers@layerware.com>
2015-12-05 09:38:52 -0600

    0.4.x snapshot dev

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject com.layerware/hugsql "0.3.1"
+(defproject com.layerware/hugsql "0.4.0-SNAPSHOT"
   :description "A Clojure library for embracing SQL"
   :url "https://github.com/layerware/hugsql"
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [com.layerware/hugsql-core "0.3.1"]
-                 [com.layerware/hugsql-adapter-clojure-java-jdbc "0.3.1"]]
+                 [com.layerware/hugsql-core "0.4.0-SNAPSHOT"]
+                 [com.layerware/hugsql-adapter-clojure-java-jdbc "0.4.0-SNAPSHOT"]]
   :aliases {"install-all" ["do" "install" ["sub" "install"]]}
   :profiles {:dev
              {:plugins [[lein-codox "0.9.0"]
@@ -14,7 +14,7 @@
                     "hugsql-adapter"
                     "hugsql-adapter-clojure-java-jdbc"
                     "hugsql-adapter-clojure-jdbc"]
-              :dependencies [[com.layerware/hugsql-adapter-clojure-jdbc "0.3.1"]]
+              :dependencies [[com.layerware/hugsql-adapter-clojure-jdbc "0.4.0-SNAPSHOT"]]
               :codox {:source-uri "http://github.com/layerware/hugsql/blob/0.3.1/{filepath}#L{line}"
                       :output-path "../gh-pages"
                       :source-paths ["hugsql-core/src"
