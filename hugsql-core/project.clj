770bf1388fe051e17c272494dfbb29ae74d2fd61
Curtis Summers <curtis.summers@layerware.com>
2017-06-12 07:42:54 -0500

    bump deps

--- hugsql-core/project.clj
+++ hugsql-core/project.clj
@@ -5,18 +5,18 @@
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
   :scm {:dir ".."}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/tools.reader "1.0.0-beta2"]
+                 [org.clojure/tools.reader "1.0.0"]
                  [com.layerware/hugsql-adapter "0.5.0-SNAPSHOT"]]
   :profiles {:dev
              {:plugins [[lein-auto "0.1.2"]]
               :dependencies [[com.layerware/hugsql-adapter-clojure-java-jdbc "0.5.0-SNAPSHOT"]
                              [com.layerware/hugsql-adapter-clojure-jdbc "0.5.0-SNAPSHOT"]
-                             [org.postgresql/postgresql "9.4.1208"]
+                             [org.postgresql/postgresql "42.1.1"]
                              [mysql/mysql-connector-java "5.1.39"]
-                             [org.xerial/sqlite-jdbc "3.8.11.2"]
-                             [org.apache.derby/derby "10.12.1.1"]
+                             [org.xerial/sqlite-jdbc "3.18.0"]
+                             [org.apache.derby/derby "10.13.1.1"]
                              [hsqldb/hsqldb "1.8.0.10"]
-                             [com.h2database/h2 "1.4.192"]]
+                             [com.h2database/h2 "1.4.196"]]
               :global-vars {*warn-on-reflection* false
                             *assert* false}}
              :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
